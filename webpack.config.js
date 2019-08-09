c4543792e9a744a888473424917c3d44f250c2d8
Sergey Kirpik <sergeykirpik@gmail.com>
2019-08-09 19:22:29 +0300

    Error Webpack Overlay Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const ErrorOverlayPlugin = require(`error-overlay-webpack-plugin`);
 
 module.exports = {
   mode: `development`,
@@ -7,12 +8,14 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
+  plugins: [new ErrorOverlayPlugin()],
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),  // Где искать сборку
     publicPath: `http://localhost:8080/`,         // Веб адрес сборки
     compress: true,
     // Автоматическая перезагрузка страницы
-    watchContentBase: true
+    watchContentBase: true,
+    overlay: true
   }
 };
